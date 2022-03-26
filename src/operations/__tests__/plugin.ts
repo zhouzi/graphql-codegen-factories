974b311e585e11c5531332b19533fdc0cf5bca71
Gabin Aureche <hello@gabinaureche.com>
2022-03-26 17:07:02 +0100

    Add test case for namespace

--- src/operations/__tests__/plugin.ts
+++ src/operations/__tests__/plugin.ts
@@ -2,7 +2,7 @@ import { buildSchema, parse } from "graphql";
 import { plugin } from "../plugin";
 
 describe("plugin", () => {
-  it("should create factories for operations", async () => {
+  it("should generate factories for operations", async () => {
     const schema = buildSchema(/* GraphQL */ `
       type User {
         id: ID!
@@ -44,6 +44,38 @@ describe("plugin", () => {
       schema,
       [{ location: "CreateUser.graphql", document: ast }],
       {}
+    );
+    expect(output).toMatchSnapshot();
+  });
+
+  it("should ignore the namespace for the operations types", async () => {
+    const schema = buildSchema(/* GraphQL */ `
+      type User {
+        id: ID!
+        username: String!
+      }
+
+      type Query {
+        users: [User!]!
+      }
+
+      type Mutation {
+        createUser(username: String): User!
+      }
+    `);
+    const ast = parse(/* GraphQL */ `
+      mutation CreateUser($username: String) {
+        createUser(username: $username) {
+          id
+          username
+        }
+      }
+    `);
+
+    const output = await plugin(
+      schema,
+      [{ location: "CreateUser.graphql", document: ast }],
+      { namespacedImportName: "types.ts" }
     );
     expect(output).toMatchSnapshot();
   });
